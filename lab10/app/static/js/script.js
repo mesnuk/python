05d27f15f6718454bcc15530239d4d864c77f488
Mesnuk <samsuser570@gmail.com>
2023-12-03 17:21:29 +0200

    remove space

--- lab10/app/static/js/script.js
+++ lab10/app/static/js/script.js
@@ -3,7 +3,6 @@ function closeAlert() {
     alertMessage.style.display = 'none';
 }
 
-
 document.getElementById('picture').addEventListener('change', function() {
     var fileName = this.value.split('\\').pop();
     document.getElementById('custom-file-label').innerText = fileName;
